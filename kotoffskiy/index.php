ec0f43c096ddeb95b9297fd522b1de28e0b6831e
sveeetch <svitlichniy.dm@gmail.com>
2017-10-27 02:19:23 +0300

    kotoffskiy - new
    kotoffskiy - new site (concept)
    based on bootstrap

---
+++ kotoffskiy/index.php
@@ -0,0 +1,106 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Ветеринарная клиника "Доктор Котоffский"</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="callback.css">
+	<link rel="stylesheet" href="style.sass">
+	<link rel="stylesheet" href="timeline.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+  </head>
+  <body>
+  	
+		<?php include"nav.php"; ?>
+	
+  	<div class="container-fluid cover ">
+      <div class="w-50 pl-5 mt-5 pt-5">
+  		<h1 class="display-4 text-white mt-5 pt-5">Ветеринарная клиника "Доктор Котоffский"</h1>
+  		<p class="lead text-light">Профессиональная помощь в области лечения инфекционных заболеваний наших четверолапых друзей  			
+  		</p>
+		  <a class="btn btn-orange btn-lg p-3" href="#" role="button">Записаться на приём</a>
+    </div>
+  	</div>
+    <div class="container">
+    	<div class="row">
+			<div class="form-group row">
+			  <label for="example-date-input" class="col-2 col-form-label">Date</label>
+			  <div class="col-10">
+			    <input class="form-control" type="date" value="2011-01-01" id="">
+			  </div>
+			</div>
+		</div>
+    <div class="row my-5 py-5">
+      <div class="col">
+      <h2>
+        Заметили такие симптомы у своего любимца?
+      </h2>
+      <ul>
+        <li>отказ от еды,</li>
+        <li>плохой аппетит,</li>
+        <li>потеря веса или резкий набор массы;</li>
+        <li>вялость или агрессия, нервозность;</li>
+        <li>покраснения, шелушение кожи;</li>
+        <li>появление залысин;</li>
+        <li>выделения из глаз, носа;</li>
+        <li>повышенная или пониженная температура;</li>
+        <li>кровь в выделениях, затрудненное мочеиспускание или вовсе его прекращение; диарея, рвота</li>
+      </ul>
+      </div>
+      <div class="col">
+      <h2 class="text-uppercase">
+        Вам стоит знать!
+      </h2>
+      <p>Если один из этих симптомов сошлись с симптомами вашего животного - то вероятнее всего, что ваш питомец уже заболел.</p>
+      <p>В таком случае стоит действовать без промедления, и сразу обратиться к специалисту. Сэкономьте ваше время деньги!</p>
+      </div>
+    </div>
+    	<div class="row">
+    		<div class="col">
+    			<h2>Some text</h2>
+    			<p>Description
+    				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    			</p>
+    		</div>
+    		<div class="col">
+    			<h2>Some text</h2>
+    			<p>Description
+    				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    			</p>
+    		</div>
+    		<div class="col">
+    			<h2>Some text</h2>
+    			<p>Description
+    				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    			</p>
+    		</div>
+    		<div class="col">
+    			<h2>Some text</h2>
+    			<p>Description
+    				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    			</p>
+    		</div>
+    	</div>
+    </div>
+<div class="container">
+  <div class="row">
+    <div class="col-2"></div>
+    <div class="col-8">
+    <?php include"timeline.php"; ?>
+    </div>
+    <div class="col-2"></div>
+  </div>
+  
+</div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    <?php include"callback.php"; ?>
+  </body>
+</html>
---
+++ kotoffskiy/nav.php
@@ -0,0 +1,15 @@
+		
+		<nav class="navbar fixed-top navbar-expand-sm navbar-dark bg-dark">
+		  <a class="navbar-brand" href="#"><img src="logo.png" alt="" width="50px"></a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+		  <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+		    <div class="navbar-nav ">
+		      <a class="nav-item nav-link text-uppercase mx-3" href="#">Домой</a>
+		      <a class="nav-item nav-link text-uppercase mx-3" href="#">Услуги</a>
+		      <a class="nav-item nav-link text-uppercase mx-3" href="#">Блог</a>
+		      <a class="nav-item nav-link text-uppercase mx-3" href="#">Контакты</a>
+		    </div>
+		  </div>
+		</nav>
