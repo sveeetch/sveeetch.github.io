e568ece3050767bbd8cb4141ed859bb71fa735f3
sveeetch <svitlichniy.dm@gmail.com>
2017-12-26 14:48:28 +0200

    котоф

--- koto/js/smoothScroll.js
+++ koto/js/smoothScroll.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
   // Add smooth scrolling on all links inside the navbar
-  $("#cover a").on('click', function(event) {
+  $("a").on('click', function(event) {
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       // Prevent default anchor click behavior
