e070c8489ee28243ba4e5ffe24aa28c3f406bbb3
sveeetch <svitlichniy.dm@gmail.com>
2018-01-29 21:24:51 +0200

    aaa1

--- astrol/main.js
+++ astrol/main.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function(){
 
 	/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'particles.json', function() {
+particlesJS.load('bg', 'particles.json', function() {
   console.log('callback - main.js config loaded');
 });
 
