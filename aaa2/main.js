8f077a316f0f5d32560b161a7c8f4e94ac74fd05
sveeetch <svitlichniy.dm@gmail.com>
2018-01-31 13:56:43 +0200

    as_1

--- aaa2/main.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function(){
-
-	/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'particles.json', function() {
-  console.log('callback - main.js config loaded');
-});
-
-})
--- astro/scriptParticle.js
+++
@@ -1,1 +0,0 @@
-particlesJS.load('bg', 'parcticle.json');
