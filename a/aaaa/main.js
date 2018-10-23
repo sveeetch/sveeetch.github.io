d3ec396e7cce6f5e618e45633cb3ce03dfabb5e5
sveeetch <svetlichniy.dm@gmail.com>
2018-10-23 22:01:46 +0300

    resume

---
+++ a/aaaa/main.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function(){
+
+	/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('callback - main.js config loaded');
+});
+
+})
